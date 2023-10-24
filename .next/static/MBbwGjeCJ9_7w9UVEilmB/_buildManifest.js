5775aed46a7bafa98c9e6bd165784292ec791e54
abraham <abraham.turnsek@gmail.com>
2023-10-24 15:16:33 +0200

    quitar espacio en numero de telefono

--- .next/static/MBbwGjeCJ9_7w9UVEilmB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/25-0606bbedcc4ae4df.js","static/css/47829f51da6f2f7b.css","static/chunks/pages/index-df0742f136f16c7e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
